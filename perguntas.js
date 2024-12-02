c4ec3295610b0ad8ee2547b81ccde2fdd6fe7f13
joaoschiavon16 <00001107198495sp@al.educacao.sp.gov.br>
2024-12-02 13:44:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Ciencia da Natureza',
+    'Por que o céu é azul?',
+    'A luz do Sol é branca, composta por várias cores. Quando a luz atravessa a atmosfera terrestre, ela interage com partículas e gases. A luz azul, que tem um comprimento de onda mais curto, é espalhada em todas as direções, fazendo com que o céu pareça azul para nossos olhos.'
+
+
+)
+
+criaCartao(
+    'Ciencia da Natureza',
+    'Quantos ossos temos no corpo?',
+    'Um adulto tem, em média, 206 ossos. Os bebês nascem com cerca de 270 ossos, mas alguns se fundem ao longo do crescimento.'
+)
+
+criaCartao(
+    'Ciencia da Natureza',
+    'Por que os gatos ronronam?',
+    'Os gatos ronronam como um mecanismo de comunicação, para expressar conforto ou até para se acalmarem em momentos de estresse ou dor. O ronronar também pode estimular a cura, já que as frequências sonoras têm efeitos positivos nos ossos e tecidos.'
+
+)
+
+criaCartao(
+    'Ciencia da Natureza',
+    'Por que sentimos coceira?',
+    'A coceira é uma reação do corpo para alertar sobre possíveis irritações na pele, como picadas de insetos ou substâncias irritantes. Coçar ajuda a remover a causa, mas também estimula o cérebro, liberando endorfinas'
+)
